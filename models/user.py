89e18d2cac3ebc927e8458ec959c6039dd699c37
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-11-25 22:50:32 +0000

    user.py converted for ORM immplementation

--- models/user.py
+++ models/user.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class User(BaseModel):
+Base = declarative_base()
+
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Iniitializes class"""
