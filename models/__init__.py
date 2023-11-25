a22f119e96abda144237160b9a96a3ba2650ce67
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-25 22:12:24 +0000

    base model added

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
 
+var = "HBNB_TYPE_STORAGE"
+env_v = os.environ.get(var)
 
-storage = FileStorage()
-storage.reload()
+if env_v =="db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 
+import os
 
-class State(BaseModel):
+env_v = os.environ.get('HBNB_TYPE_STORAGE')
+Base = declarative_base()
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    if env_v == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.__init__ import storage
+            from models.city import City
+
+            obj_list = []
+            stor = storage.all(City)
+            for key, value in stor.items():
+                if self.id == value.state_id:
+                    obj_list.append(value)
+            return obj_list
