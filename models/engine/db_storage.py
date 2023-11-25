c8fb82326d74f481d56eb7f8a949f492f779cecc
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-25 22:25:49 +0000

    db storage implementation

---
+++ models/engine/db_storage.py
@@ -0,0 +1,76 @@
+"""new engine"""
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
+import os
+
+user = os.environ.get('HBNB_MYSQL_USER')
+pswd = os.environ.get('HBNB_MYSQL_PWD')
+host = os.environ.get('HBNB_MYSQL_HOST')
+database = os.environ.get('HBNB_MYSQL_DB')
+envir = os.environ.get('HBNB_ENV')
+
+class DBStorage:
+    """implement database storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """instantiating db storage"""
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(user, pswd, host, database),
+                            pool_pre_ping=True,
+        )
+        if envir == "test":
+            metadata = MetaData()
+            metadata.drop_all(self.__engine, checkfirst=False)
+
+        def all(self, cls=None):
+            """public inst method query all class from db"""
+            from models.user import User
+            from models.state import State, Base
+            from models.city import City, Base
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import Review
+
+            if cls is None:
+                cls = [User, State, City, Amenity, Place, Review]
+                query = []
+                for cl in cls:
+                    query.extend(self.__session.query(cl).all())
+            else:
+                query.exted(self.__session.query(cls).all())
+            cls_objs = {}
+            for itm in query:
+                cls_objs[itm.to_dict()["__class__"] + "." + itm.id] = itm
+            return cls_objs
+
+        def new(self, obj):
+            """add objt to current db session"""
+            self.__session.add(obj)
+
+        def save(self):
+            """commit all changes to current db session"""
+            self.__session.commit()
+
+        def delete(self, obj=None):
+            """delet from current db session"""
+            if obj:
+                self.__session.delete(obj)
+        
+        def reload(self):
+            """method creates all tables in db"""
+            from models.user import User
+            from models.state import State, Base
+            from models.city import City, Base
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import Review
+
+            Base.metadata.create_all(self.__engine)
+
+            DBStorage.Session = scoped_session(
+                sessionmaker(bind=self.__engine, expire_on_commit=False)
+            )
+            self.__session = DBStorage,Session()
