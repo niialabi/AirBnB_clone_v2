f52e4c5cfbf74827de9447642b4df107a35aca6c
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-10 19:58:36 +0000

    try except requirement added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,20 +13,24 @@ env.user = 'ubuntu'
 @task
 def do_deploy(archive_path):
     """fbfile distributes an archive to your web servers:"""
-    if not os.path.exists(archive_path):
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        arc_name = (archive_path.split("/"))[-1]
+        file_name = (arc_name.split("."))[0]
+        dest_path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/{}".format(arc_name))
+        run("mkdir -p {}/{}".format(dest_path, file_name))
+        run("tar -xzf /tmp/{} -C {}/{}".format(arc_name, dest_path, file_name))
+        run("rm /tmp/{}".format(arc_name))
+        run("rm -rf /data/web_static/current || true")
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dest_path, file_name))
+        run("rm -rf {}{}/web_static".format(dest_path, file_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dest_path, file_name))
+        return True
+    except Exception:
         return False
-    arc_name = (archive_path.split("/"))[-1]
-    file_name = (arc_name.split("."))[0]
-    dest_path = "/data/web_static/releases/"
-    put(archive_path, "/tmp/{}".format(arc_name))
-    run("mkdir -p {}/{}".format(dest_path, file_name))
-    run("tar -xzf /tmp/{} -C {}/{}".format(arc_name, dest_path, file_name))
-    run("rm /tmp/{}".format(arc_name))
-    run("rm -rf /data/web_static/current || true")
-    run("mv {0}{1}/web_static/* {0}{1}/".format(dest_path, file_name))
-    run("rm -rf {}{}/web_static".format(dest_path, file_name))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {}{}/ /data/web_static/current".format(dest_path, file_name))
 
 
 @task
