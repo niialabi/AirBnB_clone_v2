ba31fe5d119f93b659ffda0d4edb7d9c61ebe305
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-10 19:31:00 +0000

    pycode style final clean up

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from fabric.decorators import task
 from fabric.api import *
 import os
 
@@ -8,8 +9,10 @@ import os
 env.hosts = ['54.197.82.133', '54.87.180.34']
 env.user = 'ubuntu'
 
+
+@task
 def do_deploy(archive_path):
-    """fbfile distributes an archive to your web servers, using the function do_deploy:"""
+    """fbfile distributes an archive to your web servers:"""
     if not os.path.exists(archive_path):
         return False
     arc_name = (archive_path.split("/"))[-1]
@@ -24,8 +27,9 @@ def do_deploy(archive_path):
     run("rm -rf {}{}/web_static".format(dest_path, file_name))
     run("rm -rf /data/web_static/current")
     run("ln -s {}{}/ /data/web_static/current".format(dest_path, file_name))
-   
 
+
+@task
 def do_pack():
     """fab file that compreses dir"""
     current_datetime = local('date +"%Y%m%d%H%M%S"', capture=True)
