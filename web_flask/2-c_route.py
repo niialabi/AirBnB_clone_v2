56744cce3da2ff6c5b743a451080e2c93934ebfb
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-28 04:24:34 +0000

    text replace requirement

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,6 +19,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def text(text):
     """ display variable route """
+    text = text.replace("_", " ")
     return "C {}".format(text)
 
 
