6f92748061086201eb0f7914c2cc5fb22b3c1ab1
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-28 00:50:42 +0000

    added HBNB route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,5 +11,10 @@ def hello():
     """ main page - displays Hello HBNB! """
     return "Hello HBNB!"
 
+@app.route("/hbnb")
+def hbnb():
+    """ display HBNB """
+    return "HBNB"
+
 if __name__=="__main__":
     app.run(host="0.0.0.0", port="5000")
