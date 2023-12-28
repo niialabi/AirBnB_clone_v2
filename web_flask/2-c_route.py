375a382826629bd7930aa9b7e6944d553366d1df
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-12-28 04:29:48 +0000

    refactor

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,19 +4,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
     """ main page - displays Hello HBNB! """
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     """ display HBNB """
     return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def text(text):
     """ display variable route """
     text = text.replace("_", " ")
